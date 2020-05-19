23e876487d8a533a02bc1eb04caab0e21d108138
jinshuilinxi <2329135071@qq.com>
2020-05-19 17:11:16 +0800

    添加输入框å

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { Button, Input } from 'antd'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      todoList: []
+    }
+  }
+  onChange = e => {
+    this.setState({
+      inputValue: e
+    })
+  }
+  
   render() {
+    // const { value } = this.props
     return (
       <div className="App">
-          <p>
-            hi
-          </p>
+        <Input
+          className="inputTodo"
+          placeholder="请添加一条todo"
+          allowClear
+          onChange={this.onChange} />
+        <Button type="primary">添加</Button>
       </div>
     );
   }
